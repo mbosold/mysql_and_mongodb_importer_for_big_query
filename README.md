5ee4527f3cfebf4cf40526cd53fae7b341a21a01
Mark Bosold <bosoldmark@gmail.com>
2019-04-11 18:09:33 +0200

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,179 @@
-# mysql_and_mongodb_importer_for_bigquery
+# Selective MySQL and MongoDB data import for Google BigQuery (Windows)
+
+Load data of multiple MySQL and MongoDB databases to Google BigQuery with the usage of table and column white/blacklisting.
+
+## Disclaimer
+
+* I do not consider myself a full-time programmer. I create this Python script to solve a specific problem for one of my clients. I’m thankful for every feedback to improve or simplify my code. If you would like to send some feedback, please feel free to email me at contact@datapriest.io 
+* Code documentation is aligned correctly when the files are viewed in editor “Atom.io”
+* I’m aware, that this script might not be the best option for all companies. There are many specialized software vendors (like Stitch or Fivetran) out there, to handle this workload in a managed way with SLAs
+* I’m not responsible for any results of this script e.g. bad decisions caused by wrong or bad data imports or infraction of the data privacy laws of your country
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your live system.
+
+### How it works
+1.	Dynamically read out all tables from multiple MySQL databases and MongoDB databases with separated credentials for each instance and create pandas dataframes for each table
+2.	Transform the data inside the dataframes for data privacy and BigQuery requirements (including column renaming, table and field white and black listing, column type adjusting)
+3.	Create local CSV files out of the pandas dataframes and local JSON files for the BigQuery data schema
+4.	Upload local CSV files to Google Cloud Storage
+5.	Upload data from Cloud Storage CSV files to Google BigQuery with the schema configuration of the local JSON files
+6.	Send alerts to a defined Slack channel (success or fail)
+7.	Write a local log file for errors
+
+### Files
+
+File Name        | Description
+---------------- | -------------
+import.py        | actual code including all libraries and functions
+import_config.py | config file (you need to edit this file)
+import.ipynb     | code variant as Jupyter Notebook for easier code editing, if nessary
+
+### Technical Notes
+
+* Don’t use the code without any Python and Windows Server knowledge. Try to understand the code in general to proactively take care about your data quality
+* After some internet research, I came to the conclusion, that the fasted way to load bigger datasets to Google BigQuery is to load the data via Google Cloud CSV files. A regular server is able to load 1-2 GBs of data for each table with this script.
+* For data privacy reasons, you should use the table and field whitelist feature. Therefore, it’s not recommended to sync all database tables including passwords, credit card details or other personal information without any white or blacklisting
+* Google BigQuery will not accept your MySQL and MongoDB data without any further transformation. This is why the transform_dataframe function renames columns, drops empty tables, etc..
+* Google BigQuery is able to create data schemas for the tables dynamically (configured by bigquery job settings). Unfortunately, this method is only taking a tiny part of your dataset to account for choosing the “right” field types. This can lead to wrong data types and therefore the function generate_bq_schema create the data fields dynamically out of the pandas dataframe. Unfortunately, Pandas dataframes can’t handle integer fields with NaN values and transform this fields to floats. This means, you might need to transform the related fields back to integers via Google BigQuery SQL
+* You should never store your credential information directly in the config file. You should always use an encrypted and secured way to store your credentials 
+
+### Requirements 
+
+* OS: Windows (tested with Windows Server 2016 Datacenter)
+* Language: Tested with Python 3.6.0 (Anaconda 4.3.1)
+* Packages and Libaries
+```
+pip install SQLAlchemy
+pip install google-cloud
+pip install urllib
+pip install DateTime
+pip install pymongo
+pip install pandas
+pip install datalab
+pip install numpy
+```
+
+### Setup
+
+1. Create Google Cloud JSON Key File
+* Login into Google Cloud Interface
+* Select your Google Cloud Project
+* Go to IAM Menu
+* Create new IAM with Role “Project – Editor”
+* Download JSON key file
+* Rename file “creds.json”
+* Store key file in one server folder
+* Remember the key file folder path for configuration file (later)
+
+2. Create Google Cloud Bucket
+* Go to: https://console.cloud.google.com/storage/
+* Create new bucket for CSV files
+* Remember the bucket name for configuration file (later)
+
+3. Create Google BigQuery Project
+* Login into Google Cloud Interface
+* If not existing, create new Google BigQuery project
+* Remember the BigQuery project id for configuration file (later)
+
+4. Create Google BigQuery Dataset
+* Go to: https://bigquery.cloud.google.com/
+* Create new data set (you can select EU as location)
+* Remember the BigQuery dataset id for configuration file (later)
+
+5. Create local cache folder
+* Create a folder for all JSON and CSV files on your Windows server
+* Keep in mind, that you need enough space for all (maybe large) csv files
+* Remember the cache folder path for configuration file (later)
+
+6. Create local log folder
+* Create a folder for log files on your Windows server
+* Remember the log folder path for configuration file (later)
+
+7. Store credentials as Windows system variables
+* Set environment variables for the database credentials
+* Open command prompt on your Windows server
+* Use command to set variables e.g. 
+```setx db1pw “password" /M```
+* A restart of your server is required afterwards
+
+8. Setup Slack Incoming Webhook for sending alerts via Slack
+* Create an app https://api.slack.com/apps
+* Activate “incoming webhooks”
+* Add New Webhook to Workspace
+* Select Slack channel and save URL
+* Set environment variables for the slack webhook url
+* Open command prompt on your Windows server
+* Use the following comment with your actual slack webhook url
+```setx slack_channel_1 “slack_webhook_url" /M```
+* A restart of your server is required afterwards
+
+### Configuration with import_config.py (edit file and save)
+
+If the configuration file (import_config) is set up correctly, 
+there is no further need to change anything in the actual script code (import.py). 
+As the configuration for all credentials, files, folders, whitelists and blacklists 
+can be quite complex, you should take your time to understand the architecture of the config file. 
+Edit the config file to edit the values of the following variables
+```
+general.log_file_folder,
+general.python_script_name [optional],
+general.csv_cache_folder,
+mysql.dbs.name,
+mysql.dbs.host,
+mysql.dbs.user,
+mysql.dbs.pw,
+mysql.dbs.use_table_whitelist,
+mysql.dbs.use_field_whitelist,
+mysql.dbs.use_field_blacklist,
+mysql.dbs.table_whitelist,
+mysql.dbs.field_whitelist,
+mysql.dbs.field_blacklist,
+mongo.dbs.name,
+mongo.dbs.host,
+mongo.dbs.user,
+mongo.dbs.pw,
+mongo.dbs.use_collection_whitelist,
+mongo.dbs.use_field_whitelist,
+mongo.dbs.use_field_blacklist,
+mongo.dbs.collection_whitelist,
+mongo.dbs.field_whitelist,
+mongo.dbs.field_blacklist,
+gcp.credentialfile,
+gcp.project_id,
+gcp.bucket_name,
+gcp.dataset_id,
+slack_hooks.slack_channel_1
+```
+
+### Automate hourly execution of python code with Windows Scheduler
+
+1. Copy “importer.py” and “import_config.py” to folder on Windows Server
+2. Open Windows Task Scheduler
+3. Click on “Create Basic Task”
+4. Enter a name for the new task e.g. “hourly backend import”
+5. Enter a trigger like “Daily” –> “Repeat Task every 1 hour for a duration of indefinitely”
+6. Select “Action”: “Start a program”
+7. Enter path to python.exe for “Program Script”: e.g. “C:\ProgramData\Anaconda3\python.exe”
+8. Enter path to python file in “Add arguments”: e.g. "C:\Users\user_x\import.py"
+9. Save your new scheduler task
+
+## Own Ideas for further development
+
+* Further modulation e.g. it’s possible to consolidate the functions “mysql_to_gcs” and “mongo_to_gcs”
+* Function for email alerts with log details
+* Workaround for Pandas handling with integers including NaN values
+* Possibility to load data in an incremental way with change keys (not a full load)
+* Web GUI instead of configuration file
+* Further data sources for data sources like ad network APIs   
+* Use Serverless architecture like Google Cloud Functions instead of Windows Server
+
+## Authors
+
+* **Mark Bosold** [Datapriest.io](https://github.com/mbosold)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ import.py
@@ -0,0 +1,270 @@
+
+# coding: utf-8
+
+# ## Google BigQuery Importer for MySQL and MongoDB Data (Windows)
+
+# In[1]:
+
+
+
+
+# Author: Mark Bosold | datapriest.io
+# Last Updated: 2019/04/11
+# Status: in production
+# Current Bugs:
+# - Pandas sets integer fields to float, if NAN values exists, this fields have to be transformed with BigQuery Views
+
+# In[2]:
+
+# Import nessary libaries and load config file
+from sqlalchemy import create_engine
+from google.cloud import storage
+from google.cloud import bigquery
+from urllib import request, parse
+from datetime import datetime
+from pymongo import MongoClient
+import json
+import csv
+import pandas as pd
+import os
+import datalab.storage as gcs
+import import_config as config
+import pymongo
+import numpy as np
+import sys
+
+
+# In[3]:
+
+# Function to connect to MySQL database, creates engine, create Panda dataframes for all tables, transform data and creates csv files in Google Cloud Storage
+def mysql_to_gcs():
+    # Iterate through all existing mysql databases of config file
+    for idx, mysql_database in enumerate(config.mysql['dbs']):
+        # Connect to mysql db and load table and field whitelist and blacklist configuration
+        mysql_client_url = 'mysql+mysqldb://'+mysql_database['user']+':'+mysql_database['pw']+'@'+mysql_database['host']+"/"+mysql_database['name']
+        mysql_engine = create_engine(mysql_client_url)
+        mysql_table_names = mysql_engine.table_names()
+        use_field_whitelist = mysql_database['use_field_whitelist']
+        field_whitelist = mysql_database['field_whitelist']
+        use_field_blacklist = mysql_database['use_field_blacklist']
+        field_blacklist = mysql_database['field_blacklist']
+        # Iterate through all tables of current mysql database
+        for mysql_table in mysql_table_names:
+            # Check if whitelisting is active and if current table is listed in the whitelist
+            if (mysql_database['use_table_whitelist'] == False) or (mysql_database['use_table_whitelist']== True and mysql_table in mysql_database['table_whitelist']):
+                # Select table data of mysql db
+                query='SELECT * FROM '+mysql_table
+                # Google BigQuery doesnt allow "-" in table names, so we replace it with underscores
+                db_filename = mysql_database['name'].replace("-", "_")
+                # Create file names and paths for CSV and JSON export files
+                file_name_csv = 'mysql_'+db_filename+'_'+mysql_table+'.csv'
+                file_name_json = 'mysql_'+db_filename+'_'+mysql_table+'.json'
+                file_name_csv_path=os.path.join(config.general['csv_cache_folder'],file_name_csv)
+                file_name_json_path=os.path.join(config.general['csv_cache_folder'],file_name_json)
+                # Create dataframe for table
+                table_data = pd.read_sql_query(query, mysql_engine)
+                # Move to next table if MySQL tables is empty
+                if len(table_data.index) == 0:
+                    continue
+                # Transform dataframe with given function
+                table_data = transform_dataframe(table_data,mysql_table,use_field_whitelist,field_whitelist,use_field_blacklist,field_blacklist)
+                # Create BigQuery data schema for transformed dataframe with given function
+                table_data_schema = generate_bq_schema(table_data, default_type='STRING')
+                # Local export of csv file
+                table_data.to_csv(file_name_csv_path, sep='|', quoting=csv.QUOTE_NONNUMERIC, index=False, encoding='utf8')
+                # Local export to json file
+                with open(file_name_json_path, 'w') as fp:
+                    json.dump(table_data_schema, fp)
+                # Upload local CSV File to Google Cloud Storage
+                upload_to_google_cloud_storage(file_name_csv,file_name_csv_path)
+
+
+# In[4]:
+
+# Function to connect to MongoDB database, creates engine, create Panda dataframes for all tables, transform data and creates csv files in Google Cloud Storage
+def mongo_to_gcs():
+    # Iterate through all existing MongoDB databases of config file
+    for idx, mongo_database in enumerate(config.mongo['dbs']):
+        # Connect to MongoDB and load collection and field whitelist and blacklist configuration
+        mongo_client_url = 'mongodb+srv://'+mongo_database['user']+':'+mongo_database['pw']+'@'+mongo_database['host']
+        mongo_client = pymongo.MongoClient(mongo_client_url, 27017)
+        mongo_db = mongo_client[mongo_database['name']]
+        mongo_collection_names = mongo_db.list_collection_names()
+        use_field_whitelist = mongo_database['use_field_whitelist']
+        field_whitelist = mongo_database['field_whitelist']
+        use_field_blacklist = mongo_database['use_field_blacklist']
+        field_blacklist = mongo_database['field_blacklist']
+        # Iterate through all collections of MongoDB database
+        for mongo_collection in mongo_collection_names:
+            # Check if whitelisting is active and if current collection is listed in the whitelist
+            if (mongo_database['use_collection_whitelist'] == False) or (mongo_database['use_collection_whitelist']== True and mongo_collection in mongo_database['collection_whitelist']):
+                # Select collection of mongo db
+                mongo_collection_client = mongo_db[mongo_collection]
+                cursor = mongo_collection_client.find({})
+                # Google BigQuery doesnt allow "-" in table names, so we replace it with underscores
+                db_filename = mongo_database['name'].replace("-", "_")
+                # Create file names and paths for CSV and JSON export files
+                file_name_csv = 'mongo_'+db_filename+'_'+mongo_collection+'.csv'
+                file_name_csv_path=os.path.join(config.general['csv_cache_folder'],file_name_csv)
+                file_name_json = 'mongo_'+db_filename+'_'+mongo_collection+'.json'
+                file_name_json_path=os.path.join(config.general['csv_cache_folder'],file_name_json)
+                # Create dataframe for collection
+                table_data = pd.DataFrame(list(cursor))
+                # Transform dataframe with given function
+                table_data = transform_dataframe(table_data,mongo_collection,use_field_whitelist,field_whitelist,use_field_blacklist,field_blacklist)
+                # Create BigQuery data schema for transformed dataframe with given function
+                table_data_schema = generate_bq_schema(table_data, default_type='STRING')
+                # Local export of csv file
+                table_data.to_csv(file_name_csv_path, sep='|', quoting=csv.QUOTE_NONNUMERIC, float_format='%.2f', index=False, encoding='utf8')
+                # Local export to json file
+                with open(file_name_json_path, 'w') as fp:
+                    json.dump(table_data_schema, fp)
+                # Upload local CSV File to Google Cloud Storage
+                upload_to_google_cloud_storage(file_name_csv,file_name_csv_path)
+
+
+# In[5]:
+
+# Function to read all objects of a given Google Cloud Storage Bucket and create Google BigQuery tables in a dataset for each
+def gcs_to_gbq():
+    # Connect to Google Cloud Storage and set bucket
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(config.gcp['bucket_name'])
+    blobs = bucket.list_blobs()
+    client = bigquery.Client()
+    # Connect to Google BigQuery and define BigQuery options
+    dataset_ref = client.dataset(config.gcp['dataset_id'])
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+    job_config.allow_quoted_newlines = True
+    job_config.allow_jagged_rows = True
+    job_config.autodetect = False
+    job_config.skip_leading_rows = 1
+    job_config.field_delimiter = "|"
+    job_config.source_format = bigquery.SourceFormat.CSV
+    # Iterate though all files of defined Google Cloud Storage bucket
+    for blob in blobs:
+        # Set dynamic URL for current Cloud Storage file and BigQuery schema file
+        uri = 'gs://'+config.gcp['bucket_name']+'/'+blob.name
+        file_name_json = blob.name.replace(".csv",".json")
+        file_name_json_path=os.path.join(config.general['csv_cache_folder'],file_name_json)
+        # Load JSON File for schema and set schema fields for BigQuery
+        input_json = open(file_name_json_path)
+        input_json_config = json.load(input_json)
+        job_config.schema = [
+             bigquery.SchemaField(item["name"], item["type"]) for item in input_json_config["fields"]
+        ]
+        # Set dynamic table name for Google BigQuery
+        table_name = blob.name.replace(".csv", "")
+        # Create new big query table / replace existing
+        load_job = client.load_table_from_uri(uri, dataset_ref.table(table_name), job_config=job_config)
+        assert load_job.job_type == 'load'
+        load_job.result()  # Waits for table load to complete.
+        assert load_job.state == 'DONE'
+    # Send slack message when load job done
+    send_message_to_slack(':bigquery: Backend Data Sync: Update DWH Data OK :thumbs-up-green:', config.slack_hooks['slack_channel_1'])
+
+
+# In[6]:
+
+# Function to create dynamic Google BigQuery data schema from pandas dataframe types
+# Create mapping between Pandas dataframe types and Google BigQuery field types
+def generate_bq_schema(dataframe, default_type='STRING'):
+    type_mapping = {
+        'i': 'INTEGER',
+        'b': 'BOOLEAN',
+        'f': 'FLOAT',
+        'O': 'STRING',
+        'S': 'STRING',
+        'U': 'STRING',
+        'M': 'TIMESTAMP'
+    }
+    # Use the mapping and create BigQuery schema object for dataframe
+    fields = []
+    for column_name, dtype in dataframe.dtypes.iteritems():
+        fields.append({'name': column_name,
+                       'type': type_mapping.get(dtype.kind, default_type)})
+    return {'fields': fields}
+
+
+# In[7]:
+
+# Function to apply column whitelisting/blacklisting and prepare fields for BigQuery upload
+def transform_dataframe(dataframe,table_name,use_field_whitelist,field_whitelist,use_field_blacklist,field_blacklist):
+    # Iterate through all existing columns of current dataframe
+    for column in dataframe:
+        table_column_name = table_name+"."+column
+        # use this function to export all existing columns of whitelisted database tables with the next line
+        # export_table_fields(table_column_name)
+        # When whitelist is activated for the database, drop all fields, which are not in whitelist
+        if (use_field_whitelist == True and table_column_name not in field_whitelist):
+            dataframe.drop(column, axis=1, errors='ignore', inplace=True)
+            continue
+        # When blacklist is activated for the database, drop all fields, which are in blacklist
+        if (use_field_blacklist == True and table_column_name in field_blacklist):
+            dataframe.drop(column, axis=1, errors='ignore', inplace=True)
+            continue
+        # Find all fields with '\n' and '\r' and replace them with white spaces
+        if dataframe[column].dtype != np.number:
+            dataframe[column].replace('\n',' ', inplace=True, regex=True)
+            dataframe[column].replace('\r',' ', inplace=True, regex=True)
+    # Find all fields, where the fieldname starts with "_" and remove the first character from the field name
+    dataframe.rename(columns=lambda x: x[1:] if (x[0]=='_') else x, inplace=True)
+    return dataframe
+
+
+# In[8]:
+
+# Function to send success or fail messages to configured slack channel for monitoring reasons
+def send_message_to_slack(alert_message, slack_hook):
+    post = {"text": "{0}".format(alert_message)}
+    json_data = json.dumps(post)
+    req = request.Request(slack_hook, data=json_data.encode('ascii'), headers={'Content-Type': 'application/json'})
+    resp = request.urlopen(req)
+
+
+# In[9]:
+
+# Function to write error to a local log file
+def write_error_to_log(error_message):
+    i = datetime.now()
+    logfile_name = 'log-'+i.strftime('%Y-%m-%d')+'.txt'
+    logfile_path = os.path.join(config.general['log_file_folder'],logfile_name)
+    logfile = open(logfile_path,'a+')
+    logfile.write(i.strftime('%Y-%m-%d %H:%M:%S')+' '+config.general['python_script_name']+': '+error_message+'\n')
+
+
+# In[10]:
+
+# Function to upload files to Google CLoud Storage bucket from config
+def upload_to_google_cloud_storage(file_name_csv,file_name_csv_path):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(config.gcp['bucket_name'])
+    blob = bucket.blob(file_name_csv)
+    blob.upload_from_filename(file_name_csv_path)
+
+
+# In[11]:
+
+# Help function to write all fields of whitelisted tables to a local log file for copy & pasting to fields whitelist of config file
+def export_table_fields(field):
+    logfile_name = 'fields.txt'
+    logfile_path = os.path.join(config.general['csv_cache_folder'],logfile_name)
+    logfile = open(logfile_path,'a+')
+    logfile.write(field+'\n')
+
+
+# In[12]:
+
+# Execution of complete job
+try:
+    # execute mysql_to_gcs function for all mysql databases
+    mysql_to_gcs()
+    # execute mongo_to_gcs function for all mongo databases
+    mongo_to_gcs()
+    # execute gcs_to_gbq function for google cloud storage buckets
+    gcs_to_gbq()
+except Exception as em:
+    # Send message to slack for failed job and write error log
+    send_message_to_slack(':bigquery: Backend Data Sync: Update DWH Data Failed :no_entry:', config.slack_hooks['slack_channel_1'])
+    write_error_to_log(str(em))
